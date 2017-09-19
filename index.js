1ec6eab3b3e5982552886179927d799a945ce2d1
Josh Junon <Qix-@users.noreply.github.com>
2017-09-19 18:55:58 +0200

    New spec (#1)
    * refactor to new spec format
    
    * remove custom handlers
    
    * update readme to reflect new spec

--- index.js
+++ index.js
@@ -1,51 +1,36 @@
-function defaultUnknownHandler(name, /* , val */) {
-	throw new Error(`Unknown or unexpected option: ${name}`);
-}
+function zarg(argv, opts) {
+	const result = {_: []};
 
-function zarg(argv, opts, unknownHandler) {
-	if (!Array.isArray(argv)) {
-		unknownHandler = opts;
-		opts = argv;
-		argv = null;
+	/* eslint-disable default-case */
+	switch (arguments.length) {
+		case 0:
+			return result;
+		case 1:
+			opts = argv;
+			argv = null;
+			break;
 	}
-
-	if (typeof opts === 'function') {
-		unknownHandler = opts;
-		opts = null;
-	}
+	/* eslint-enable default-case */
 
 	argv = argv || process.argv.slice(2);
-	opts = opts || {};
-	unknownHandler = unknownHandler || defaultUnknownHandler;
 
+	const aliases = {};
 	const handlers = {};
-	const setType = (name, type, dest) => {
-		if (name in handlers) {
-			const odest = handlers[name][1];
-			const extended = `--${dest}` === name ? '' : `alias for --${dest}, `;
-			throw new Error(`Duplicate option configuration: ${name} (${extended}originally for --${odest})`);
-		}
-
-		handlers[name] = [type, dest];
-	};
 
 	for (const key of Object.keys(opts)) {
-		const [type, aliases] = Array.isArray(opts[key]) ? [opts[key][0], opts[key].slice(1)] : [opts[key], []];
-
-		const name = `--${key}`;
-
-		if (!type || typeof type !== 'function') {
-			throw new Error(`Type missing or not a function: ${name}`);
+		if (typeof opts[key] === 'string') {
+			aliases[key] = opts[key];
+			continue;
 		}
 
-		setType(name, type, key);
+		const type = opts[key];
 
-		for (const alias of aliases) {
-			setType(alias, type, key);
+		if (!type || (typeof type !== 'function' && !(Array.isArray(type) && type.length === 1 && typeof type[0] === 'function'))) {
+			throw new Error(`Type missing or not a function or valid array type: ${key}`);
 		}
+
+		handlers[key] = type;
 	}
-
-	const result = {_: []};
 
 	for (let i = 0, len = argv.length; i < len; i++) {
 		const arg = argv[i];
@@ -61,27 +46,46 @@ function zarg(argv, opts, unknownHandler) {
 		}
 
 		if (arg[0] === '-') {
-			const [argName, argStr] = arg[1] === '-' ? arg.split('=', 2) : [arg, undefined];
+			const [originalArgName, argStr] = arg[1] === '-' ? arg.split('=', 2) : [arg, undefined];
 
-			if (!(argName in handlers)) {
-				unknownHandler(argName, argStr);
-				continue;
+			let argName = originalArgName;
+			while (argName in aliases) {
+				argName = aliases[argName];
 			}
 
-			const [type, dest] = handlers[argName];
+			if (!(argName in handlers)) {
+				throw new Error(`Unknown or unexpected option: ${originalArgName}`);
+			}
 
+			/* eslint-disable operator-linebreak */
+			const [type, isArray] = Array.isArray(handlers[argName])
+				? [handlers[argName][0], true]
+				: [handlers[argName], false];
+			/* eslint-enable operator-linebreak */
+
+			let value;
 			if (type === Boolean) {
-				result[dest] = true;
+				value = true;
 			} else if (argStr === undefined) {
 				if (argv.length < i + 2 || (argv[i + 1].length > 1 && argv[i + 1][0] === '-')) {
-					const extended = `--${dest}` === argName ? '' : ` (alias for --${dest})`;
-					throw new Error(`Option requires argument: ${argName}${extended}`);
+					const extended = originalArgName === argName ? '' : ` (alias for ${argName})`;
+					throw new Error(`Option requires argument: ${originalArgName}${extended}`);
 				}
 
-				result[dest] = type(argv[i + 1], argName, result[dest]);
+				value = type(argv[i + 1], argName, result[argName]);
 				++i;
 			} else {
-				result[dest] = type(argStr, argName, result[dest]);
+				value = type(argStr, argName, result[argName]);
+			}
+
+			if (isArray) {
+				if (result[argName]) {
+					result[argName].push(value);
+				} else {
+					result[argName] = [value];
+				}
+			} else {
+				result[argName] = value;
 			}
 		} else {
 			result._.push(arg);
